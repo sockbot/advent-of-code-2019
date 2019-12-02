2fe75569471305ec4c37da27716c19d5a51a261f
Andy Chan <github@sockbot.com>
2019-12-01 18:44:48 -0800

    add php solution

--- 1.js
+++ 1.js
@@ -5,7 +5,8 @@ const calculateFuel = mass => {
   const fuel = Math.floor(mass / 3) - 2;
   if (fuel > 0) {
     return fuel + calculateFuel(fuel);
-  } else return 0;
+  }
+  return 0;
 };
 
 axios
