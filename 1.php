a30fa251495505068cc4c4bc6d6c4ce33a5d974e
Andy Chan <github@sockbot.com>
2019-12-01 21:47:46 -0800

    solve puzzle 2

--- 1.php
+++ 1.php
@@ -1,24 +1,29 @@
-<?php 
-  // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-  // $dotenv->load();
-  // echo $_ENV['AOC_COOKIE']; 
-  $file = fopen("1-input.txt", "r") or die("Unable to open file!");
-  $input = fread($file,filesize("1-input.txt"));
-  fclose($file);
+<?php
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
+// echo $_ENV['AOC_COOKIE'];
+($file = fopen("1-input.txt", "r")) or die("Unable to open file!");
+$input = fread($file, filesize("1-input.txt"));
+fclose($file);
 
-  $data = explode("\n", $input);
+$data = explode("\n", $input);
 
-  function calculateFuel($mass) {
-    $fuel = floor($mass / 3) - 2;
-    if ($fuel > 0) {
-      return $fuel + calculateFuel($fuel);
-    }
-    return 0;
+function calculateFuel($mass)
+{
+  $fuel = floor($mass / 3) - 2;
+  if ($fuel > 0) {
+    return $fuel + calculateFuel($fuel);
   }
+  return 0;
+}
 
-  $total = array_reduce($data, function($carry, $item) {
+$total = array_reduce(
+  $data,
+  function ($carry, $item) {
     return $carry + calculateFuel($item);
-  }, 0);
-  
-  print_r($total . "\n")
+  },
+  0
+);
+
+print_r($total . "\n");
 ?>
---
+++ 2.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const axios = require("axios");
+
+axios
+  .get("https://adventofcode.com/2019/day/2/input", {
+    headers: {
+      Cookie: `session=${process.env.AOC_COOKIE}`
+    }
+  })
+  .then(res => {
+    const data = res.data.split(",").map(x => parseInt(x, 10));
+
+    let instructions = [...data];
+
+    const runComputer = (opcode, input1, input2, output) => {
+      let value = 0;
+      if (opcode === 99) {
+        // console.log("ANSWER FOUND:", instructions[0]);
+        return instructions[0];
+      } else if (opcode === 1) {
+        value = instructions[input1] + instructions[input2];
+      } else if (opcode === 2) {
+        value = instructions[input1] * instructions[input2];
+      }
+      instructions[output] = value;
+    };
+
+    for (let noun = 0; noun < 100; noun++) {
+      for (let verb = 0; verb < 100; verb++) {
+        instructions[1] = noun;
+        instructions[2] = verb;
+        for (let i = 0; i < instructions.length; i = i + 4) {
+          const answer = runComputer(
+            instructions[i],
+            instructions[i + 1],
+            instructions[i + 2],
+            instructions[i + 3]
+          );
+          if (answer === 19690720) {
+            console.log(100 * noun + verb);
+          }
+        }
+        instructions = [...data];
+      }
+    }
+  });
